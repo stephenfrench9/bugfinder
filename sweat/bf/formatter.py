d28ae060e6b82069073439d874206f7e74752178
Stephen French <stephenfrench9@gmail.com>
2019-07-17 19:53:05 -0700

    fix pep8 violations in sweat/bf/

--- sweat/bf/formatter.py
+++ sweat/bf/formatter.py
@@ -1,14 +1,6 @@
-import requests
-from flask import Flask
-from flask import request
-from lib.tracing import init_tracer
-from time import sleep
-from opentracing.ext import tags
-from opentracing.propagation import Format
-from random import randint
-from elasticsearch import Elasticsearch
-import pprint
 import datetime
+from elasticsearch import Elasticsearch
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -26,73 +18,55 @@ def powerset(seq):
             yield item
 
 
-def http_get(port, path, param, value):
-    url = 'http://elasticsearch:%s/%s' % (port, path)
-
-    span = tracer.active_span
-    span.set_tag(tags.HTTP_METHOD, 'GET')
-    span.set_tag(tags.HTTP_URL, url)
-    span.set_tag(tags.SPAN_KIND, tags.SPAN_KIND_RPC_CLIENT)
-    headers = {}
-    tracer.inject(span, Format.HTTP_HEADERS, headers)
-
-    r = requests.get(url, params={param: value}, headers=headers, timeout=1)
-    assert r.status_code == 200
-    return r.text
-
-
 @app.route("/format")
-def format():
-    # es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+def conditional_distributions():
     es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
 
-    size = 1000
-    threshold = 60000
+    # Hard-coded Bugfinder parameters.
+    # How many traces in the past does Bugfinder analyze? What is the
+    # definition of a slow response?
+    trace_window = 1000  # traces
+    threshold = 60000  # milliseconds
 
-    # d = str(datetime.datetime.today()).split()[0]
-    # d = "2019-04-30"
     d = str(datetime.datetime.utcnow()).split()[0]
     # get list of all traces
-    res = es.search(index='jaeger-span-' + d, size=size)
-    a = res['hits']['hits']
-    # pp = pprint.PrettyPrinter(indent=0)
-    # pp.pprint(a[0])
+    res = es.search(index='jaeger-span-' + d, size=trace_window)
+    all_traces = res['hits']['hits']
 
-    # get useful lists
+    # Break lists of traces into lists of services, trace_ids, and spans_ids.
     services = []
     traces = []
     spans = []
-    for trace in a:
+    for trace in all_traces:
         service = trace['_source']['process']['serviceName']
-        traceID = trace['_source']['traceID']
-        spanID = trace['_source']['spanID']
+        trace_id = trace['_source']['traceID']
+        span_id = trace['_source']['spanID']
         if service not in services:
             services.append(service)
-        if traceID not in traces:
-            traces.append(traceID)
-        if spanID not in spans:
-            spans.append(spanID)
+        if trace_id not in traces:
+            traces.append(trace_id)
+        if span_id not in spans:
+            spans.append(span_id)
 
-    # build events dictionary
+    # Build events dictionary. Given a trace_id and a service, the 'events'
+    # dictionary will tell you the duration of that service call.
     events = {}
     for i in range(len(traces)):
         search = {"query": {"match": {'traceID': traces[i]}}}
-        res = es.search(index='jaeger-span-' + d, body=search, size=size)
-        a = res['hits']['hits']  # all the spans to do with this trace
+        res = es.search(index='jaeger-span-' + d, body=search,
+                        size=trace_window)
+        all_traces = res['hits']['hits']  # all the spans to do with this trace
 
-        for trace in a:
+        for trace in all_traces:
             service = trace['_source']['process']['serviceName']
-            traceID = trace['_source']['traceID']
-            spanID = trace['_source']['spanID']
+            trace_id = trace['_source']['traceID']
+            # span_id = trace['_source']['spanID']
             duration = trace['_source']['duration']
-            if traceID not in events.keys():
-                events[traceID] = {}
-            events[traceID][service] = duration
+            if trace_id not in events.keys():
+                events[trace_id] = {}
+            events[trace_id][service] = duration
 
-    # for trace in events.keys():
-    #     print(events[trace])
-
-    # identify slow traces
+    # Identify slow traces.
     for trace in events.keys():
         slow = False
         for service in events[trace].keys():
@@ -100,29 +74,30 @@ def format():
                 slow = True
         events[trace]['slow'] = slow
 
-    # Get counts for (path, speed)
+    # Get counts for (path, speed).
     slow_counts = {}
     fast_counts = {}
     for trace in traces:
-        # find all the marginal arguments for ONE trace
+        # Find all the marginal arguments for ONE trace.
         traces_in_span = []
-        traces = list(events.keys())
+        # traces = list(events.keys())
         for key in events[trace].keys():
             if key != 'slow':
                 traces_in_span.append(key)
 
         traces_in_span = sorted(traces_in_span)
 
-        # see all the powersets
-        powersets = powerset(traces_in_span)
+        # Generate all combinations of services for which you want
+        # performance metrics.
+        power_sets = powerset(traces_in_span)
         marginals_args = []
-        marginalargset = "whatever"
-        while (marginalargset != "donee"):
-            marginalargset = next(powersets, 'donee')
-            if marginalargset != [] and marginalargset != 'donee':
-                marginals_args.append(marginalargset)
+        marginal_arg_set = "initialized"
+        while marginal_arg_set != "done":
+            marginal_arg_set = next(power_sets, 'done')
+            if marginal_arg_set != [] and marginal_arg_set != 'done':
+                marginals_args.append(marginal_arg_set)
 
-        # Add all the marginal_args to the distribution
+        # Add all the marginal_args to the distribution.
         for args in marginals_args:
             if events[trace]['slow']:
                 if ",".join(args) not in list(slow_counts.keys()):
@@ -135,31 +110,37 @@ def format():
                 else:
                     fast_counts[",".join(args)] += 1
 
-    # Calculate conditional distributions
+    # Calculate the conditional distributions.
     cond_dist = {}
     for slow_args, slow_count in slow_counts.items():
         if slow_args in list(fast_counts.keys()):
-            cond_dist[slow_args] = slow_count / (slow_count + fast_counts[slow_args])
+            cond_dist[slow_args] = slow_count / \
+                                   (slow_count + fast_counts[slow_args])
         else:
             cond_dist[slow_args] = slow_count / slow_count
 
+    # Reformat conditional distributions to look like
+    # [[service1, service2, .98], ..] etc.
     keys = []
     for k, v in cond_dist.items():
         keys.append(k.split(','))
         keys[-1].append(v)
 
+    # Place single services at the front of the list, and larger combinations
+    # of services at the end.
     diagnosis = sorted(keys, key=len)
 
+    # Build the final result string to be returned.
     result = ""
     for diagnosis in diagnosis:
         services = ' & '.join(diagnosis[:-1])
-        result = result + "P(" + str(services) + ") = " + str(round(diagnosis[-1], 2)) + "<br/>"
+        result = result + \
+                 "P(" + str(services) + ") = " + \
+                 str(round(diagnosis[-1], 2)) + \
+                 "<br/>"
 
-    print(result)
     return '<font size="22">' + result + '</font>'
 
 
 if __name__ == "__main__":
-    # app.run(port=8081)
     app.run(debug=True, host='0.0.0.0', port=5000)
-    # format()
--- sweat/bf/propertime.py
+++
@@ -1,6 +0,0 @@
-import datetime
-u = str(datetime.datetime.utcnow()).split()[0]
-reg = str(datetime.datetime.today()).split()[0]
-
-print("utc: %s" % u)
-print("doesn't work: %s" % reg)
